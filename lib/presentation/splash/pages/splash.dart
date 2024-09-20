5ef83ca095ba7d890cedcdd70785784c0161e31c
Kamal Abdullayev <kamalabdullayev@MacBook-Air-Kamal.local>
2024-09-20 14:29:32 +0400

    splash

--- lib/presentation/splash/pages/splash.dart
+++ lib/presentation/splash/pages/splash.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nutri_fit/core/configs/assets/app_images.dart';
 import 'package:nutri_fit/presentation/intro_sign/pages/get_started.dart';
 
 class SplashPage extends StatefulWidget {
@@ -17,13 +18,40 @@ class _SplashPageState extends State<SplashPage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.white,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Align(
+            alignment: Alignment.center,
+            child: SizedBox(
+              height: 200,
+              width: 200,
+              child: Image.asset(
+                AppImages.logo,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Image.asset(
+            AppImages.yazi,
+            height: 60,
+            width: 300,
+          ),
+          SizedBox(
+            height: 130,
+          ),
+        ],
+      ),
     );
   }
 
   Future<void> redirect() async {
-    await Future.delayed(const Duration(seconds: 2));
+    await Future.delayed(const Duration(seconds: 3));
     Navigator.pushReplacement(
       context,
       MaterialPageRoute(
